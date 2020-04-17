7b7f76f0873531786e436c49235676a2b051a511
DInuwan97 <kalubowila.dc25132@live.com>
2020-04-17 22:22:21 +0530

    2nd Commit

--- app.js
+++ app.js
@@ -35,16 +35,34 @@ app.directive('datetimepicker', function(){
 
 app.controller('MyEvntOrganizerAppController', function($scope){
     $scope.date = moment()
-    
-    
+  
+
+    function calculateDif(eDate){
+
+        var dateString = moment(eDate,"DD/MM/YYYY");
+        var dateObj = dateString.toDate().toLocaleDateString("en-US")
+
+        var curDate = new Date().toLocaleDateString("en-US");
+        var givenDate = new Date(dateObj).toLocaleDateString("en-US")
+
+        console.log("Todays Date : " ,new Date());
+        console.log("Sent Date : " ,((new Date(dateObj)).toLocaleDateString("en-US")));
+
+        if((new Date()).toLocaleDateString("en-US") <= (new Date(dateObj)).toLocaleDateString("en-US")){
+            var dif = Math.abs(new Date(dateObj) - new Date());
+            console.log('Dif is : ',dif)
+            return dif;
+            
+         }else{
+            var dif = -1
+            console.log('Dif is : ',dif)
+            return dif;
+         } 
 
 
+    }
 
-    // $scope.compareDates = function(date1,date2){
-    //     if (date1>date2) return ("Date1 > Date2");
-    //   else if (date1<date2) return ("Date2 > Date1");
-    //   else return ("Date1 = Date2"); 
-    //  }
+
 
     function checkEvents(eDate){
 
@@ -54,14 +72,15 @@ app.controller('MyEvntOrganizerAppController', function($scope){
         var dateString = moment(eDate,"DD/MM/YYYY");
         var dateObj = dateString.toDate().toLocaleDateString("en-US")
  
-         console.log("give date : ", (new Date(dateObj)).toLocaleDateString("en-US"));
+         console.log("give date : ", ((new Date(dateObj)).toLocaleDateString("en-US")));
          console.log('current date', (new Date()).toLocaleDateString("en-US"))
+
  
          if((new Date()).toLocaleDateString("en-US") <= (new Date(dateObj)).toLocaleDateString("en-US")){
-            console.log('Current Date is greater')
-             return true;
-         }else{
             console.log('Given Date is greater')
+            return true;
+         }else{
+            console.log('Current Date is greater')
              return false;
          } 
 
@@ -73,22 +92,19 @@ app.controller('MyEvntOrganizerAppController', function($scope){
         $scope.events.splice(removeEvent1,1); 
         var today  = new Date();
 
-
         $scope.model={
             eDateTime1:'13/08/2020 14:49'
         }
-
-
      
     }
 
-  
         $scope.updateEvent = function(event) {
-            // Example with 2 arguments
+           
             angular.copy(event, {
                 eName:$scope.updateEvents.eName,
                 eDateTime:$scope.upateEvents.eDateTime,
-                isExpire: checkEvents($scope.upateEvents.eDateTime)
+                isExpire: checkEvents($scope.upateEvents.eDateTime),
+                dif:calculateDif($scope.upateEvents.eDateTime)
             });
 
             $scope.updateEvents.eName="";
@@ -101,7 +117,8 @@ app.controller('MyEvntOrganizerAppController', function($scope){
             id:$scope.events.length + 1,
             eName:$scope.newEvents.eName,
             eDateTime:$scope.newEvents.eDateTime,
-            isExpire:checkEvents($scope.newEvents.eDateTime)
+            isExpire:checkEvents($scope.newEvents.eDateTime),
+            dif:calculateDif($scope.newEvents.eDateTime)
         });
 
         $scope.newEvents.eName="";
@@ -115,24 +132,29 @@ app.controller('MyEvntOrganizerAppController', function($scope){
             id:1,
             eName:"BirthdayPary",
             eDateTime:"16/04/2020 14:45",
-            isExpire: checkEvents("16/04/2020 14:45")
+            isExpire: checkEvents("16/04/2020 14:45"),
+            dif:calculateDif("16/04/2020 14:45")
         },
         {
             id:2,
             eName:"Wedding",
             eDateTime:"14/06/2020 14:45",
-            isExpire: checkEvents("14/06/2020 14:45")
+            isExpire: checkEvents("14/06/2020 14:45"),
+            dif:calculateDif("14/06/2020 14:45")
         },
         {
             id:3,
             eName:"BMovie",
             eDateTime:"12/06/2020 14:36",
-            isExpire: checkEvents("12/06/2020 14:36")
+            isExpire: checkEvents("12/06/2020 14:36"),
+            dif:calculateDif("12/06/2020 14:36")
         },
         {
+            id:4,
             eName:"Temple",
             eDateTime:"20/06/2020 13:00",
-            isExpire: checkEvents("20/06/2020 13:00")
+            isExpire: checkEvents("20/06/2020 13:00"),
+            dif:calculateDif("20/06/2020 13:00")
         }
     ];
 
